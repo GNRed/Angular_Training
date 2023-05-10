5bf53c921949355c566924e974c9527c0f982129
GNRed <120460076+GNRed@users.noreply.github.com>
2023-05-10 18:06:40 +0530

    Add files via upload

---
+++ Day3Assignment2.js
@@ -0,0 +1,13 @@
+// Prompt
+var abc = prompt("Enter the number");
+console.log(abc);
+//Alert
+alert("this is an elert");
+//Confirm
+var b = confirm("Do you want to save the changes?");
+if (b) {
+    console.log("Your changes have been saved");
+}
+else {
+    console.log("Your changes were not saved");
+}

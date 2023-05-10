94876b1843c25b8d2e968f388dc6a6e04168d55b
GNRed <120460076+GNRed@users.noreply.github.com>
2023-05-10 18:26:39 +0530

    Add files via upload

---
+++ Day3/Assignment3/Day3Assignment3.ts
@@ -0,0 +1,11 @@
+enum directions {
+    East,
+    West,
+    North,
+    South
+  }
+
+console.log(directions[0])
+console.log(directions[1])
+console.log(directions[2])
+console.log(directions[3])

806adbbcc43a51638dfb594f3e23a8b50884f9ab
GNRed <120460076+GNRed@users.noreply.github.com>
2023-05-10 18:24:51 +0530

    Add files via upload

---
+++ Day3/Assignment2/Day3Assignment2.ts
@@ -0,0 +1,17 @@
+// Prompt
+var abc = prompt("Enter the number")
+console.log(abc)
+
+//Alert
+alert("this is an elert")
+
+//Confirm
+let b=confirm("Do you want to save the changes?")
+
+if(b){
+    console.log("Your changes have been saved")
+}
+else
+{
+    console.log("Your changes were not saved")
+}
